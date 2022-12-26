c8285c77106544d572953f647e1dd8fc3f6ca964
Abdallah Saleh <72446432+Abdallahsa@users.noreply.github.com>
2022-12-26 17:58:24 +0200

    using list without matrix and with weighted

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include<vector>
 using namespace std;
-typedef vector<vector<int>> Graphs;
+struct Edge{
+    int cost;
+    int to;
+};
+typedef vector<vector<Edge>> Graphs;
 
-void add_direct(vector<vector<int>>& Graph,int from ,int to){
-    Graph[from].push_back(to);
+void add_direct(vector<vector<Edge>>& Graph,int from,int to,int cost){
+    Graph[from].push_back({to,cost});
 
 }
-void add_undirect(vector<vector<int>>& Graph,int from ,int to){
-    Graph[from].push_back(to);
-    Graph[to].push_back(from);
+void add_undirect(vector<vector<Edge>>& Graph,int from ,int to,int cost){
+    Graph[from].push_back({cost,to});
+    Graph[to].push_back({cost,from});
 
 }
 void print_dirct_graph(Graphs& graphs){
     int size=graphs.size();
+
     for (int from = 0; from < size; ++from) {
         cout<<"Node "<<from<<" has neighbors: ";
         for (int to = 0; to <(int)graphs[from].size(); ++to) {
-          cout<<graphs[from][to]<<" ";
+          cout<<graphs[from][to].to<<" "<<"are weighted "<<graphs[from][to].cost;
 
         }
         cout<<endl;
@@ -28,10 +33,11 @@ int main() {
     int node,edges;
     cin>>node>>edges;
  Graphs graphs(node);
+ Edge  edge;
     for (int i = 0; i < edges; ++i) {
-        int to,from;
-        cin>>from>>to;
-        add_direct(graphs,from,to);
+        int to,from,weighted;
+        cin>>from>>to>>weighted;
+        add_direct(graphs,from,to,weighted);
     }
     print_dirct_graph(graphs);
     return 0;
